070713b7d563328a4b3398cff4be7b9a55e4c302
joziasmartini <jozias.martini@gmail.com>
2025-09-26 20:54:59 -0300

    feat: import new components in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
+import JobList from "@/components/JobList";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
-      <Navbar/>
+      <Navbar />
       <Header />
+      <JobList />
     </main>
   );
 }
