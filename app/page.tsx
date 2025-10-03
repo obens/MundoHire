5833410b9923e0c5d6a3df398669ed41b47abc2b
Obenson Maurice <obensonmaurice23@gmail.com>
2025-10-03 20:39:10 -0300

    minhas alterações

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <main className="flex flex-col">
-      <Navbar/>
+      <Navbar />
       <Header />
     </main>
   );
